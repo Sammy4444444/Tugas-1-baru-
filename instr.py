1c2cd30e2fd03647f8b5e5547b011bafb4c4f83f
Sammy4444444 <kaxi4300@gmail.com>
2026-04-26 20:27:06 +0700

    penambahan QQDoubleValidator, QIntValidator, QFontQTimer, QTime, QLocale

--- instr.py
+++ instr.py
@@ -1,1 +1,8 @@
 # write a code for saving text instructions for the app
+txt_index = 'Indeks Roufier: '
+txt_workheart = 'Kinerja jantung: '
+txt_res1 = "rendah. Temui dokter Anda segera!"
+txt_res2 = "memuaskan. Temui dokter Anda!"
+txt_res3 = "rata-rata. Mungkin ada baiknya menemui dokter Anda untuk diperiksa."
+txt_res4 = "di atas purata"
+txt_res5 = "tinggi"
--- second_win.py
+++ second_win.py
@@ -1,12 +1,12 @@
 # write a code for the second screen of app
 #IMPORT
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale 
 from PyQt5.QtWidgets import (
     QApplication, QWidget,
     QHBoxLayout, QVBoxLayout,
     QGroupBox, QRadioButton,
-    QPushButton, QLabel, QListWidget, QLineEdit)
-
+    QPushButton, QLabel, QListWidget, QLineEdit,  )
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont
 from instr import *
 from final_win import *
 
@@ -31,10 +31,22 @@ txt_starttest3 = 'Start the final test'
 txt_timer = ''
 txt_age = 'Full years:'
 
+class Experiment():
+    def __init__(self, age, test1, test2, test3):
+        self.age = age
+        self.t1 = test1
+        self.t2 = test2
+        self.t3 = test3
+  
+
+        
+        
+        
+
 #-----class------
 class TestWin(QWidget):
     def __init__(self):
-        ''' the window in which the survey is being conducted '''
+        
         super().__init__()
         
         self.initUI()
@@ -52,7 +64,7 @@ class TestWin(QWidget):
     def connects(self):
         self.btn_next.clicked.connect(self.next_click)
 
-    ''' sets what the window will look like (label, size, location) '''
+    
     def set_appear(self):
         self.setWindowTitle(txt_title)
         self.resize(win_width, win_height)
@@ -98,4 +110,59 @@ class TestWin(QWidget):
 
     def next_click(self):
         self.hide()
-        self.fw = FinalWin()
+        self.exp = Experiment(int(self.line_age.text()), self.line_test1.text(), self.line_test2.text(), self.line_test2.text())
+        self.fw = FinalWin(self.exp)
+    
+    def time_test(self):
+        global time
+        time = QTime(0, 0, 15)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer1Event)
+        self.timer.start(1000)
+
+    def timer_sits(self):
+        global time
+        time = QTime(0, 0, 30)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer2Event)
+        self.timer.start(1500)
+
+    def timer_final(self):
+        global time
+        time = QTime(0, 1, 0)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer3Event)
+        self.timer.start(1000)
+
+    def timer1Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.text_timer.setText(time.toString("hh:mm:ss"))
+        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+        self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timer.stop()
+
+    def timer3Event(self):
+        global time
+        timer = time.addSecs(-1)
+        self.text_timer.setText(time.toString("hh:mm:ss"))
+        if int(time.toString("hh:mm:ss")[6:8]) >= 45:
+             self.text_timer.setStyleSheet("color: rgb(0,255,0)")
+        elif int(time.toString("hh:mm:ss")[6:8]) <= 15:
+            self.text_timer.setStyleSheet('color: rgb(0,255,0)')
+        else:
+             self.text_timer.setStyleSheet('color: rgb(0,0,0)')
+        self.text_timer.toSring(QFont("Times", 36, QFont.Bold)) 
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timer.stop()
+        
+    def connects(self):
+        self.btn_next.clicked.connect(self.next_click)
+        self.btn_test1.clicked.connect(self.timer_test)
+        self.btn_test2.clicked.connect(self.timer_sits)
+        self.btn_test3.clicked.connect(self.timer_final)
+        
+        
+        
+        
